85e6ad99098e510180f4928717b0bcecca3896dc
Michał Walczak <devvalczak@gmail.com>
2024-01-18 21:59:24 +0100

    feature: integration for wp non customizable form input names

--- js/custom-form-action.js
+++ js/custom-form-action.js
@@ -1,5 +1,6 @@
 jQuery(document).ready(($) => {
     const formTable = JSON.parse(customFormActionParams.formTable);
+    const formNames = JSON.parse(customFormActionParams.formNames);
     const integrations = JSON.parse(customFormActionParams.integrations);
     const apiEndpoint = customFormActionParams.apiEndpoint;
 
@@ -16,6 +17,7 @@ jQuery(document).ready(($) => {
             '<td><input type="text" name="custom_form_action_form_table[' + newId + '][branch]" value="" /></td>' +
             '<td><input class="monospace" spellcheck="false" type="text" name="custom_form_action_form_table[' + newId + '][callback]" value="" /></td>' +
             '<td><textarea spellcheck="false" class="scriptTextarea monospace" name="custom_form_action_form_table[' + newId + '][script]"></textarea></td>' +
+            '<td><input type="checkbox" name="custom_form_action_form_table[' + newId + '][prevent]"/></td>' +
             '<td><a href="#" class="remove-form-row">Usuń</a></td>' +
             '</tr>';
     }
@@ -43,13 +45,22 @@ jQuery(document).ready(($) => {
     // integrations
     Object.values(formTable).forEach((integration) => {
         const formId = integration.form_id;
-        const formObject = $('#' + formId);
+        const formContainerObject = $('#' + formId);
+        let formObject = null;
 
-        // Sprawdź, czy formularz istnieje na stronie
+        if (formContainerObject.is('form')) {
+            formObject = formContainerObject
+        } else {
+            formObject = formContainerObject.find('form');
+        }
+
+        // Check is formObject correct
         if (formObject.length) {
-            // prevent default actions on form
-            formObject.removeAttr('action')
-            formObject.removeAttr('method')
+            // prevent default actions on target form
+            if (integration.prevent) {
+                formObject.removeAttr('action')
+                formObject.removeAttr('method')
+            }
 
             // inject response handler script
             const script = document.createElement('script')
@@ -59,21 +70,24 @@ jQuery(document).ready(($) => {
 
         formObject.on( "submit", (e) => {
             let requestData = {}
-
             try {
                 requestData = {
-                    'name': e.target.name.value,
-                    'surname': e.target.surname.value,
-                    'email': e.target.email.value,
-                    'source': 'wwww',
+                    'name': e.target[formNames.name].value,
+                    'surname': e.target[formNames.surname].value,
+                    'email': e.target[formNames.email].value,
+                    'source': e.target[formNames.source].value,
                     'sourceDetails': window.location.protocol + window.location.host,
                     'branch': integration.branch,
                     'callback': integration.callback,
-                    'phone': e.target.phone?.value ?? '',
-                    'message': e.target.message?.value ?? '',
+                    'phone': e.target[formNames.phone]?.value ?? '',
+                    'message': e.target[formNames.message]?.value ?? '',
                 }
             } catch (e) {
                 console.error(`Invalid input names on form ${formId}`, e);
+                return false;
+            }
+
+            if (!requestData.name || !requestData.surname || !requestData.source || !requestData.branch) {
                 return false;
             }
 
--- thtg-api-integrator.php
+++ thtg-api-integrator.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Thtg API form integrator
-Version: 1.0
+Version: 1.1
 Author: devvalczak
 */
 
@@ -17,6 +17,7 @@ function custom_form_action_enqueue_scripts() {
     // Pass PHP variables to JavaScript
     wp_localize_script('custom-form-action', 'customFormActionParams', array(
         'formTable' => json_encode(get_option('custom_form_action_form_table')),
+        'formNames' => json_encode(get_option('custom_form_action_form_names')),
         'apiEndpoint' => get_option('custom_form_action_api_endpoint'),
         'pluginUrl' => esc_js(plugin_dir_url(__FILE__)),
         'integrations' => json_encode(array("client")),
@@ -35,9 +36,10 @@ add_filter("plugin_action_links_$plugin", 'custom_form_action_settings_link');
 // Register settings
 function custom_form_action_register_settings() {
     add_option('custom_form_action_form_table', array());
+    add_option('custom_form_action_form_names', array());
     add_option('custom_form_action_api_endpoint', '');
     register_setting('custom_form_action_settings_group', 'custom_form_action_form_table');
-    register_setting('custom_form_action_settings_group', 'custom_form_action_api_key');
+    register_setting('custom_form_action_settings_group', 'custom_form_action_form_names');
     register_setting('custom_form_action_settings_group', 'custom_form_action_api_endpoint');
 }
 add_action('admin_init', 'custom_form_action_register_settings');
@@ -51,6 +53,7 @@ add_action('admin_menu', 'custom_form_action_settings_page');
 // Settings page content
 function custom_form_action_settings_page_content() {
     $integrations = array('client');
+    $formNames = get_option('custom_form_action_form_names', array());
     ?>
     <div class="wrap thtgIntegrator">
         <h2>Thtg API Form Integrator Settings</h2>
@@ -61,72 +64,111 @@ function custom_form_action_settings_page_content() {
             <table class="form-table">
                 <tr valign="top">
                     <th scope="row">Endpoint API (public)</th>
-                    <td class="apiEndpointWrapper">
+                    <td class="apiEndpointWrapper" colspan="6">
                         <input class="apiEndpointInput" type="text" name="custom_form_action_api_endpoint" value="<?php echo esc_attr(get_option('custom_form_action_api_endpoint', '')); ?>" />
                     </td>
                 </tr>
             </table>
-                <h3>Integracje</h3>
-                <table class="form-table" id="form-table">
-                    <thead>
+            <h3>Nazwy pól formularzy dla integracji 'client'</h3>
+            <table class="form-table">
+                <thead>
+                    <tr>
+                        <th scope="row">name</th>
+                        <th scope="row">surname</th>
+                        <th scope="row">email</th>
+                        <th scope="row">source</th>
+                        <th scope="row">phone</th>
+                        <th scope="row">message</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <input type="text" name="custom_form_action_form_names[name]" value="<?php echo esc_attr($formNames['name']); ?>" />
+                        </td>
+                        <td>
+                            <input type="text" name="custom_form_action_form_names[surname]" value="<?php echo esc_attr($formNames['surname']); ?>" />
+                        </td>
+                        <td>
+                            <input type="text" name="custom_form_action_form_names[email]" value="<?php echo esc_attr($formNames['email']); ?>" />
+                        </td>
+                        <td>
+                            <input type="text" name="custom_form_action_form_names[source]" value="<?php echo esc_attr($formNames['source']); ?>" />
+                        </td>
+                        <td>
+                            <input type="text" name="custom_form_action_form_names[phone]" value="<?php echo esc_attr($formNames['phone']); ?>" />
+                        </td>
+                        <td>
+                            <input type="text" name="custom_form_action_form_names[message]" value="<?php echo esc_attr($formNames['message']); ?>" />
+                        </td>
+                    </tr>
+                <tbody>
+            </table>
+            <h3>Integracje</h3>
+            <table class="form-table" id="form-table">
+                <thead>
+                    <tr valign="top">
+                        <th scope="col">ID formularza</th>
+                        <th scope="col">Integracja</th>
+                        <th scope="col">ID oddziału (branch)</th>
+                        <th scope="col">Callback</th>
+                        <th scope="col">JS</th>
+                        <th scope="col">Usuń domyślne akcje</th>
+                        <th scope="col"></th>
+                    </tr>
+                </thead>
+                <tbody id="tableBody">
+                    <?php
+                        $formTable = get_option('custom_form_action_form_table', array());
+                        if (!empty($formTable)) foreach ($formTable as $index => $formRow) {
+                    ?>
                         <tr valign="top">
-                            <th scope="col">ID formularza</th>
-                            <th scope="col">Integracja</th>
-                            <th scope="col">ID oddziału (branch)</th>
-                            <th scope="col">Callback</th>
-                            <th scope="col">JS</th>
-                            <th scope="col"></th>
+                            <td>
+                                <input type="text" spellcheck="false" name="custom_form_action_form_table[<?php echo $index; ?>][form_id]" value="<?php echo esc_attr($formRow['form_id']); ?>" />
+                            </td>
+                            <td>
+                                <select name="custom_form_action_form_table[<?php echo $index; ?>][integration]">
+                                    <?php
+                                        foreach ($integrations as $integration) {
+                                            $selected = ($integration === $formRow['integration']) ? 'selected="selected"' : '';
+                                            echo "<option value='$integration' $selected>$integration</option>";
+                                        }
+                                    ?>
+                                </select>
+                            </td>
+                            <td>
+                                <input type="text" name="custom_form_action_form_table[<?php echo $index; ?>][branch]" value="<?php echo esc_attr($formRow['branch']); ?>" />
+                            </td>
+                            <td>
+                                <input class="monospace" spellcheck="false" type="text" name="custom_form_action_form_table[<?php echo $index; ?>][callback]" value="<?php echo esc_attr($formRow['callback']); ?>" />
+                            </td>
+                            <td>
+                                <textarea class="scriptTextarea monospace" spellcheck="false" name="custom_form_action_form_table[<?php echo $index; ?>][script]"><?php echo esc_attr($formRow['script']); ?></textarea>
+                            </td>
+                            <td>
+                                <input type="checkbox" name="custom_form_action_form_table[<?php echo $index; ?>][prevent]" <?php echo isset($formRow['prevent']) && $formRow['prevent'] ? 'checked' : ''; ?> />
+                            </td>
+                            <td>
+                                <a href="#" class="remove-form-row">Usuń</a>
+                            </td>
                         </tr>
-                    </thead>
-                    <tbody id="tableBody">
                         <?php
-                            $formTable = get_option('custom_form_action_form_table', array());
-                            if (!empty($formTable)) foreach ($formTable as $index => $formRow) {
+                    } else {
                         ?>
                             <tr valign="top">
-                                <td>
-                                    <input type="text" spellcheck="false" name="custom_form_action_form_table[<?php echo $index; ?>][form_id]" value="<?php echo esc_attr($formRow['form_id']); ?>" />
-                                </td>
-                                <td>
-                                    <select name="custom_form_action_form_table[<?php echo $index; ?>][integration]">
-                                        <?php
-                                            foreach ($integrations as $integration) {
-                                                $selected = ($integration === $formRow['integration']) ? 'selected="selected"' : '';
-                                                echo "<option value='$integration' $selected>$integration</option>";
-                                            }
-                                        ?>
-                                    </select>
-                                </td>
-                                <td>
-                                    <input type="text" name="custom_form_action_form_table[<?php echo $index; ?>][branch]" value="<?php echo esc_attr($formRow['branch']); ?>" />
-                                </td>
-                                <td>
-                                    <input class="monospace" spellcheck="false" type="text" name="custom_form_action_form_table[<?php echo $index; ?>][callback]" value="<?php echo esc_attr($formRow['callback']); ?>" />
-                                </td>
-                                <td>
-                                    <textarea class="scriptTextarea monospace" spellcheck="false" name="custom_form_action_form_table[<?php echo $index; ?>][script]"><?php echo esc_attr($formRow['script']); ?></textarea>
-                                </td>
-                                <td>
-                                    <a href="#" class="remove-form-row">Usuń</a>
-                                </td>
+                                <td colspan="6" id="noRows">Brak aktywnych integracji</td>
                             </tr>
-                            <?php
-                        } else {
-                            ?>
-                                <tr valign="top">
-                                    <td colspan="6" id="noRows">Brak aktywnych integracji</td>
-                                </tr>
-                            <?php
-                        }
-                        ?>
-                    </tbody>
-                </table>
-                <a href="#" class="add-form-row">Dodaj nową integrację</a>
+                        <?php
+                    }
+                    ?>
+                </tbody>
+            </table>
+            <a href="#" class="add-form-row">Dodaj nową integrację</a>
             <?php submit_button(); ?>
         </form>
         <div class="readme monospace">
             Integracja `client`: <br />
-            Wymagane pola: `name`, `surname`, `email` <br />
+            Wymagane pola: `name`, `surname`, `email`, `source`<br />
             Pola opcjonalne: `phone`, `message`
         </div>
     </div>
